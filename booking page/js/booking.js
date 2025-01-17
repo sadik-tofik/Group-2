128d8a2d9ad3ae4429fbdd00aa5dbd7c42744d42
Natnael B. <natnaelb382@gmail.com>
2025-01-17 14:51:21 +0300

    Add files via upload

---
+++ booking page/js/booking.js
@@ -0,0 +1,47 @@
+function toggleDropdown() {
+    const dropdownItems = document
+        .getElementById('dropdownItems');
+    dropdownItems.style.display = dropdownItems.style.display === 'block' ? 'none' : 'block';
+}
+
+document
+    .getElementById('dropdownSearch')
+    .addEventListener('input', function () 
+    {
+        const filter = this
+            .value
+            .toUpperCase();
+        const items = document
+            .getElementsByClassName('dropdown-item');
+
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const txtValue = item.textContent || item.innerText;
+            item
+                .style
+                .display = txtValue
+                    .toUpperCase()
+                    .includes(filter) ? '' : 'none';
+        }
+    });
+
+document
+    .addEventListener('click', function (event)
+    {
+        const dropdown = document
+            .querySelector('.dropdown');
+        if (!dropdown.contains(event.target))
+        {
+            document
+                .getElementById('dropdownItems')
+                .style
+                .display = 'none';
+        }
+    });
+
+document
+    .getElementById('dropdownItems')
+    .addEventListener('click', function (event) 
+    {
+        event.stopPropagation();
+    });
